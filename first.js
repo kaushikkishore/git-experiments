574f558664c788f981635a30d7bd49281b893566
Kaushik <kk12391@gmail.com>
2023-06-07 19:05:55 +0530

    sixth

--- first.js
+++ first.js
@@ -7,3 +7,5 @@ console.log('third commit on the feature branch.');
 console.log('fourth commit on the feature branch.');
 
 console.log('fifth commit on the feature branch.');
+
+console.log('sixth commit on the feature branch.');
