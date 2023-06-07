593b70876ce867f51f56ff689070cb9b2e596ac0
Kaushik <kk12391@gmail.com>
2023-06-07 22:11:01 +0530

    devide added

--- two.js
+++ two.js
@@ -1,19 +1,3 @@
-console.log('first master change');
-
-console.log('second master change');
-
-console.log('third master change');
-
-console.log('forth master change');
-
-console.log('fifth master change');
-
-console.log('sixth master change');
-
-console.log('seven master change');
-
-console.log('eight master change');
-
-console.log('nine master change');
-
-console.log('ten master change');
+function divide(first, second) {
+    return first / second;
+}
