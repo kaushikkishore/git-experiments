6d2eddba12092394134956763f33af1a1a2fd7dc
Kaushik <kk12391@gmail.com>
2023-06-09 18:17:16 +0530

    Added employee details and updated readme to get base hash from where branch was created

---
+++ employee.js
@@ -0,0 +1,15 @@
+function getEmployeeName() {
+    // get the employee name from the database.
+}
+
+function getEmployeeDepartment() {
+    // Get employee department from the database. 
+}
+
+function getEmployeeSalary() {
+    // get the employee salary from the databse 
+}
+
+function getEmployeeCTC() {
+    // Get the employee ctc
+}
