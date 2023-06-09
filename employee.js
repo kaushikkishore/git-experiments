fd23118aad7288e62ea644ec064e4744b1679ff9
Kaushik <kk12391@gmail.com>
2023-06-09 18:42:14 +0530

    added bad line 1 2 3 4

--- employee.js
+++ employee.js
@@ -1,15 +1,19 @@
 function getEmployeeName() {
     // get the employee name from the database.
+    // This line will conflict in future
 }
 
 function getEmployeeDepartment() {
     // Get employee department from the database. 
+    // Changed here also some bad comments
 }
 
 function getEmployeeSalary() {
     // get the employee salary from the databse 
+    // Some more bad lines here
 }
 
 function getEmployeeCTC() {
     // Get the employee ctc
+    // Some more here in CTC also.
 }
