8d23c2f4b9a13d203c99ebc28b619a13a18e4bb5
Kaushik <kk12391@gmail.com>
2023-06-13 13:47:24 +0530

    update transactions

--- transactions.js
+++ transactions.js
@@ -2,6 +2,14 @@ function get() {
     // Get the transactions data
 }
 
-function create(profile) {
+function create(transaction) {
     // Create a transactions here
+}
+
+function update(id, transaction) {
+    // update the transaction data now. 
+}
+
+function remove(id) {
+    // Delete the transaction
 }
